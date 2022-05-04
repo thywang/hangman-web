bc0dee8a1349f88e57d00cb8f97fbd95ef28c6f1
Tina Wang <tina@metrist.io>
2022-05-04 16:09:07 -0400

    added implementation for updating letters in tally

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -76,7 +76,7 @@ defmodule Hangman.Impl.Game do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters:    reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort,
     }
   end
@@ -85,7 +85,16 @@ defmodule Hangman.Impl.Game do
     { game, tally(game) }
   end
 
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+
+  end
+
   defp maybe_won(true), do: :won
   defp maybe_won(_),    do: :good_guess
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _letter), do: "_"
 
 end
