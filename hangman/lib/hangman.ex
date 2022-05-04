ab1aab2c4207ad9a35cba5a2dab8e34620f4ff7c
Tina Wang <tina@metrist.io>
2022-05-04 14:46:39 -0400

    organized states into a separate file type.ex

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,20 +1,13 @@
 defmodule Hangman do
 
   alias Hangman.Impl.Game
-
-  @type   state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  alias Hangman.Type
   @opaque game  :: Game.t
-  @type   tally :: %{
-    turns_left: integer,
-    game_state: state,
-    letters: list(String.t),
-    used: list(String.t),
-  }
 
   @spec new_game() :: game
   defdelegate new_game(), to: Game
 
-  @spec make_move(game, String.t) :: { game, tally }
+  @spec make_move(game, String.t) :: { game, Type.tally }
   # make_move will return a new game state and the tally
   def make_move(_game, _guess) do
   end
