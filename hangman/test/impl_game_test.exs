e8b07e4ad612ca5519a592b3aec2f2aaea125efd
Tina Wang <tina@metrist.io>
2022-05-04 16:03:04 -0400

    added tests for checking what a good/bad guess should be

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -50,4 +50,53 @@ defmodule HangmanImplGameTest do
     assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
   end
 
+  test "we recognize a letter in the word" do
+    game = Game.new_game("wombat")
+    { _game, tally } = Game.make_move(game, "m")
+    assert tally.game_state == :good_guess
+    { _game, tally } = Game.make_move(game, "t")
+    assert tally.game_state == :good_guess
+  end
+
+  test "we recognize a letter not in the word" do
+    game = Game.new_game("wombat")
+    { _game, tally } = Game.make_move(game, "x")
+    assert tally.game_state == :bad_guess
+
+    { _game, tally } = Game.make_move(game, "t")
+    assert tally.game_state == :good_guess
+
+    { _game, tally } = Game.make_move(game, "y")
+    assert tally.game_state == :bad_guess
+  end
+
+# molly
+  test "can handle a sequence of moves" do
+    [
+      # guess | state    turns    letters                    used
+      [ "a",  :bad_guess,  6,   [ "_", "_", "_", "_", "_"], [ "a" ]],
+      [ "a",  :bad_guess,  6,   [ "_", "_", "_", "_", "_"], [ "a" ]],
+      [ "o",  :good_guess, 6,   [ "_", "o", "_", "_", "_"], [ "a", "o" ]],
+      [ "x",  :bad_guess,  5,   [ "_", "o", "_", "_", "_"], [ "a", "o", "x" ]],
+
+    ]
+    |> test_sequence_of_moves()
+  end
+
+  def test_sequence_of_moves(script) do
+    game = Game.new_game("molly")
+    Enum.reduce(script, game, &check_one_move/2)
+  end
+
+  defp check_one_move([ guess, state, turns, letters, used ], game) do
+    { game, tally } = Game.make_move(game, guess)
+
+    assert tally.game_state == state
+    assert tally.turns_left == turns
+    assert tally.letters    == letters
+    assert tally.used       == used
+
+    game
+  end
+
 end
