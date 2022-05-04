45e487b70f3a01af44f0a7e56cc4a98ea57335c7
Tina Wang <tina@metrist.io>
2022-05-04 11:24:59 -0400

    specified types for new_game() and make_move() functions

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,9 +1,19 @@
 defmodule Hangman do
+  @type state :: :initialiting | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game :: any
+  @type tally :: %{
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t),
+  }
 
+  @spec new_game() :: game
   def new_game do
   end
 
+  @spec make_move(game, String.t) :: { game, tally }
   # make_move will return a new game state and the tally
-  def make_move(game, guess) do
+  def make_move(_game, _guess) do
   end
 end
