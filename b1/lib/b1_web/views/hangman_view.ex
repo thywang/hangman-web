dfecad251918cab88244106459d1977678977058
Tina Wang <tina@metrist.io>
2022-05-06 14:59:42 -0400

    added some CSS stylings and display appropriate message

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,6 +1,20 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
 
+  @status_fields %{
+    initializing: { "initializing", "Guess the word, a letter at a time" },
+    good_guess:   { "good-guess",   "Good guess!" },
+    bad_guess:    { "bad-guess",    "Sorry, that's a bad guess" },
+    won:          { "won",          "Congratulations, you won!" },
+    lost:         { "lost",         "Sorry, you lost" },
+    already_used: { "already-used", "You already used that letter" }
+  }
+
+  def move_status(status) do
+    { class, msg } = @status_fields[status]
+    "<div class='status #{class}'>#{msg}</div>" |> raw()
+  end
+
   def figure_for(0) do
     ~s{
         _______
