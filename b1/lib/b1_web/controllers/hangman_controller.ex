04ceb31c4f2db0c93cd5cd5f55c1df1fb2ce5662
Tina Wang <tina@metrist.io>
2022-05-06 11:55:45 -0400

    added display for current tally

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -4,4 +4,12 @@ defmodule B1Web.HangmanController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def new(conn, _params) do
+    game = Hangman.new_game()
+    tally = Hangman.tally(game)
+    put_session(conn, :game, game)
+    render(conn, "game.html", tally: tally)
+  end
+
 end
