5d349cc91ef9d176eb26a07d82417edef36374ef
Tina Wang <tina@metrist.io>
2022-05-06 09:20:35 -0400

    took out the random failure in dictionary server

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -13,9 +13,6 @@ defmodule Dictionary.Runtime.Server do
   end
 
   def random_word() do
-    if :rand.uniform < 0.33 do
-      Agent.get(@me, fn _ -> exit(:crash) end)
-    end
     Agent.get(@me, &WordList.random_word/1)
   end
 
