22aa55a3c116737042b53e11088d1d749c3d7ce7
Tina Wang <tina@metrist.io>
2022-05-06 14:22:47 -0400

    added hangman figure to the game

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,3 +1,109 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
+
+  def figure_for(0) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+     /|\\      |
+     / \\      |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(1) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+     /|\\      |
+     /        |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(2) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+     /|\\      |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(3) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+     /|       |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(4) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+      |       |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(5) do
+    ~s{
+        _______
+      |      \\|
+      O       |
+              |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(6) do
+    ~s{
+        _______
+      |      \\|
+              |
+              |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+  def figure_for(7) do
+    ~s{
+        _______
+             \\|
+              |
+              |
+              |
+              |
+              |
+           ___|_
+    }
+  end
+
+
 end
